01f4135ba864f284add9405a87fe844a9238976f
nishams63 <nisham556655@gmail.com>
2026-04-06 13:38:35 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   transpilePackages: ['three'],
+
+  experimental: {
+    esmExternals: 'loose',
+  },
 };
 
 module.exports = nextConfig;
